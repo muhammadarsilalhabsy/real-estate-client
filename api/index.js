de8a9fdac5384635ea10644707b32d580b2db8a9
muhammadarsilalhabsy <muhammadarsilalhabsy@gmail.com>
2023-10-02 22:13:13 +0800

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(console.log("Success connect to database"))
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
+
+//  connect to mongo atlas
 
 app.get("/", (req, res) => {
   res.send("Jalan gaes");
